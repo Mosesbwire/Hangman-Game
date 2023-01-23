8e59c3eaaed5f95b753a159112d5acaf992ce885
Mosesbwire <mosesdev92@gmail.com>
2023-01-23 14:07:01 +0000

    Build hangman game in C

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+/**
+  * count_words - counts number of words in file
+  * @file: file to be read
+  * Return: total number of words in file
+  */
 int count_words(FILE *file)
 {
 	unsigned int total = 0;
@@ -12,7 +19,7 @@ int count_words(FILE *file)
 		exit(98);
 	}
 
-	while(!feof(file))
+	while (!feof(file))
 	{
 		ch = fgetc(file);
 		if (ch == ',')
@@ -27,6 +34,14 @@ int count_words(FILE *file)
 
 }
 
+/**
+  * get_word - finds word in a file according to its index in file
+  * @file: file to be read
+  * @str: pointer to string
+  * @index: position of string
+  * Return: pointer to str
+  */
+
 char *get_word(FILE *file, char *str, int index)
 {
 	char ch;
@@ -39,7 +54,7 @@ char *get_word(FILE *file, char *str, int index)
 		exit(98);
 	}
 
-	while(!feof(file))
+	while (!feof(file))
 	{
 		ch = fgetc(file);
 		if (ch >= 97 && ch <= 122)
@@ -56,7 +71,6 @@ char *get_word(FILE *file, char *str, int index)
 			{
 				str[0] = 0;
 			}
-
 		}
 
 		if (total == index)
@@ -66,23 +80,84 @@ char *get_word(FILE *file, char *str, int index)
 	}
 
 	return (str);
-
-
 }
+
+/**
+  * print_answer - prints to stdout
+  * @answer: pointer to string to print
+  * @len: length of string
+  * Return: void
+  */
+
+void print_answer(char *answer, int len)
+{
+	int x;
+
+	for (x = 0; x < len; x++)
+	{
+		if (answer[x] >= 97 && answer[x] <= 122)
+		{
+			printf("%c ", answer[x]);
+		} else
+		{
+			printf("__ ");
+		}
+	}
+	printf("\n");
+}
+/**
+  * is_answer - checks that user input is part of quiz string
+  * @quiz: pointer to string
+  * @answer: pointer to string
+  * @input: user char input
+  * Return: 0 if input is not in quiz, 1 if input is in quiz
+  */
+int is_answer(char *quiz, char *answer, int input)
+{
+	int x, len, return_value = 0;
+
+	len = strlen(quiz);
+	for (x = 0; x < len; x++)
+	{
+		if (answer[x] == input)
+		{
+			return (1);
+		}
+		if (input == *(quiz + x))
+		{
+			answer[x] = input;
+			(return_value = 1);
+		}
+	}
+
+	return (return_value);
+}
+/**
+  * main - entry point
+  * Return: 0 (always success)
+  */
+
 int main(void)
 {
 	FILE *file;
-	int size;
 	char *quiz;
+	char *answer;
+	char user_input;
+	int num_of_guesses = 5;
+	int _is_answer, words;
+	time_t t1;
+	int continue_game = 1;
+	char input;
 
 	file = fopen("Hangman_wordbank", "r");
+	while (continue_game)
+	{
 
 	if (file == NULL)
 	{
 		printf("Cannot read file \n");
 		return (1);
 	}
-	
 	quiz = malloc(sizeof(char *) * 50);
 
 	if (quiz == NULL)
@@ -91,12 +166,62 @@ int main(void)
 		return (2);
 	}
 
-	quiz = get_word(file, quiz, 1000);
-	printf("Word is %s\n", quiz);
-	fseek(file, 0, SEEK_SET);
-	size = count_words(file);
-	printf("Total words in file %d\n", size);
+	answer = malloc(sizeof(char *) * 50);
 
+	if (answer == NULL)
+	{
+		printf("Failed to allocate memory\n");
+		return (2);
+	}
+	
+	words = count_words(file);
+	fseek(file, 0, SEEK_SET);
+
+	srand((unsigned int) time(&t1));
+
+	quiz = get_word(file, quiz, (rand() % words + 1));
+	print_answer(answer, strlen(quiz));
+
+	while (num_of_guesses >= 1)
+	{
+		printf("Enter your guess: ");
+		scanf(" %c", &user_input);
+		_is_answer = is_answer(quiz, answer, user_input);
+
+		if (!_is_answer)
+		{
+			num_of_guesses--;
+		}
+
+		if (!strcmp(quiz, answer))
+		{
+			break;
+		}
+
+		print_answer(answer, strlen(quiz));
+	}
+
+	if (!strcmp(quiz, answer))
+	{
+		printf("You won the game\n");
+	} else
+	{
+		printf("You lost the game.\nThe correct word is: %s\n", quiz);
+	}
+
+	free(quiz);
+	free(answer);
+	printf("Enter c to play new game.\nEnter q to quit game.\n");
+	
+	scanf(" %c", &input);
+	if (input == 'q')
+	{
+		continue_game = 0;
+	}
+	
+	num_of_guesses = 5;
+
+	}
 	fclose(file);
 	return (0);
 }
